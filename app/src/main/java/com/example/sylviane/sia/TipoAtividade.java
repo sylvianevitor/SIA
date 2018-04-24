ccc75e2b6d26e796c02b30f58f97db8f341d916c
Sylviane Vitor <sylvianevitor@gmail.com>
2018-04-24 16:55:22 -0300

    Telas iniciais

--- app/src/main/java/com/example/sylviane/sia/TipoAtividade.java
+++ app/src/main/java/com/example/sylviane/sia/TipoAtividade.java
@@ -3,12 +3,11 @@ package com.example.sylviane.sia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TipoAtividade extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tipo_atividade);
     }
-
 }
