a57fb993b6e9de67df1d3938c7f22803ed85baa8
Unknown <cavichiolimariana@gmail.com>
2018-04-23 23:03:34 -0300

    Tela Inicial

---
+++ app/src/main/java/com/example/sylviane/sia/Main_Scene/MainPresenter.java
@@ -0,0 +1,28 @@
+package com.example.sylviane.sia.Main_Scene;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mariana on 23/04/18.
+ */
+
+public class MainPresenter extends AppCompatActivity {
+
+    MainView mainView = null;
+
+    public MainPresenter(MainView mainView){
+        this.mainView = mainView;
+    }
+
+    public void criarAtividade() {
+        mainView.criar();
+    }
+
+    public void selecionarAtividade() {
+        mainView.selecionar();
+    }
+
+    public void verAssistidos() {
+        mainView.ver();
+    }
+}
---
+++ app/src/main/java/com/example/sylviane/sia/Main_Scene/MainView.java
@@ -0,0 +1,11 @@
+package com.example.sylviane.sia.Main_Scene;
+
+/**
+ * Created by mariana on 23/04/18.
+ */
+
+public interface MainView {
+    void criar();
+    void selecionar();
+    void ver();
+}
