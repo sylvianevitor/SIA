daf7e6204cf1bfcb29f26c5ed29ba21163c341a5
Natasha <natashasofia@gmail.com>
2018-04-25 23:14:55 -0300

    Lista de Atividades Controller

---
+++ app/src/main/java/com/example/sylviane/sia/activities/Entity/AtividadesEntity.java
@@ -0,0 +1,18 @@
+package com.example.sylviane.sia.activities.Entity;
+
+/**
+ * Created by Natasha on 25/04/2018.
+ */
+
+public class AtividadesEntity {
+    private String nome;
+    private String infos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getInfos() {
+        return infos;
+    }
+}
