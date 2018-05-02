a5cd54a3e25a00f2aeffcc36cebcf58f34dc74a9
Luan Batista da Silva <luanbatista.ti@gmail.com>
2018-05-02 10:26:58 -0300

    Adicionar models e dao

---
+++ app/src/main/java/com/example/sylviane/sia/com/example/sylviane/sia/persist/model/Formacao.java
@@ -0,0 +1,23 @@
+package com.example.sylviane.sia.com.example.sylviane.sia.persist.model;
+
+public class Formacao {
+
+    private int Id;
+    private String formacao;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+}
