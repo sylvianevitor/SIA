556d862d3a70a2259bc575fd936c4ce2ce1233c5
Luan Batista da Silva <luanbatista.ti@gmail.com>
2018-05-14 10:16:47 -0300

    Adicionar propriedade Imagem aos componentes persist do Tema

---
+++ app/src/main/java/com/example/sylviane/sia/persist/model/AtividadeTemplate1.java
@@ -0,0 +1,15 @@
+package com.example.sylviane.sia.persist.model;
+
+import java.util.List;
+
+public class AtividadeTemplate1 extends Atividade {
+
+    private List<Template1Arquivo> Arquivos;
+
+    public boolean adicionarAquivo(Template1Arquivo arquivo) {
+
+
+
+    }
+
+}
