6653ac1130b53fe87d5fc1e6944bb1d2517ddc4d
Luan Batista da Silva <luanbatista.ti@gmail.com>
2018-05-14 10:22:49 -0300

    Corrigir caminho do pacote Persist

--- app/src/main/java/com/example/sylviane/sia/Tema_Scene/Tema_Passivo/TemaPassivoActivity.java
+++ app/src/main/java/com/example/sylviane/sia/Tema_Scene/Tema_Passivo/TemaPassivoActivity.java
@@ -3,7 +3,7 @@ package com.example.sylviane.sia.Tema_Scene.Tema_Passivo;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.sylviane.sia.Tema_Scene.Tema_Interativo.TemaInterativoView;
-import com.example.sylviane.sia.com.example.sylviane.persist.persist.model.Tema;
+import com.example.sylviane.sia.persist.model.Tema;
 
 import java.util.List;
 
