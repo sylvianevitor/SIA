736e045689827b1711b81cb31ac76ebbc4174ddc
Rafael Saito <rafaelbsaito@hotmail.com>
2018-05-17 20:44:35 -0300

    Controller do relatório pós atividade feito

--- app/src/main/java/com/example/sylviane/sia/Relatorios/RelatoriosActivity.java
+++ app/src/main/java/com/example/sylviane/sia/Relatorios/RelatoriosActivity.java
@@ -1,21 +1,36 @@
 package com.example.sylviane.sia.Relatorios;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.sylviane.sia.Main_Scene.MainActivity;
 import com.example.sylviane.sia.R;
 
 import butterknife.ButterKnife;
-
-/**
- * Created by Natasha on 26/04/2018.
- */
+import butterknife.OnClick;
 
 public class RelatoriosActivity extends AppCompatActivity implements RelatoriosView{
+
+    RelatoriosPresenter relatoriosPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relatorios);
         ButterKnife.bind(this);
+
+        relatoriosPresenter = new RelatoriosPresenter(this);
+    }
+
+    @OnClick(R.id.botao_sair)
+    public void sair_relatorios(){
+        relatoriosPresenter.voltar_tela_inicial();
+    }
+
+    @Override
+    public void sair() {
+        Intent voltarMainActivity = new Intent(RelatoriosActivity.this, MainActivity.class);
+        startActivity(voltarMainActivity);
     }
 }
