e553d8d828d67faaec243097f983bc17d78de61d
Natasha <natashasofia@gmail.com>
2018-06-06 16:44:49 -0300

    Tela de assistidos e detalhes funcionando no celular

---
+++ app/src/main/java/com/example/sylviane/sia/ListaAssistidos/OnRecyclerViewSelectedAssistidos.java
@@ -0,0 +1,12 @@
+package com.example.sylviane.sia.ListaAssistidos;
+
+import android.view.View;
+
+/**
+ * Created by Natasha on 30/05/2018.
+ */
+
+interface OnRecyclerViewSelectedAssistidos {
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
